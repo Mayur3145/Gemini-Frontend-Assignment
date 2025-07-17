c7bde634eb884c9d19bcb3d6c867081f9f2b1d26
Mayur Chaudhari <2019mayur.chaudhari@ves.ac.in>
2025-07-18 00:43:17 +0530

    deployment update3

--- src/utils/countryApi.ts
+++ src/utils/countryApi.ts
@@ -7,28 +7,52 @@ export interface Country {
   flag: string;
 }
 
+// Define interfaces for the API response
+interface CountryName {
+  common: string;
+  official: string;
+}
+
+interface CountryFlags {
+  svg: string;
+  png: string;
+}
+
+interface CountryIdd {
+  root: string;
+  suffixes: string[];
+}
+
+interface ApiCountry {
+  name: CountryName;
+  flags: CountryFlags;
+  idd: CountryIdd;
+}
+
 export const fetchCountries = async (): Promise<Country[]> => {
   try {
-    const response = await axios.get('https://restcountries.com/v3.1/all?fields=name,flags,idd');
-    
+    const response = await axios.get<ApiCountry[]>(
+      'https://restcountries.com/v3.1/all?fields=name,flags,idd'
+    );
+
     return response.data
-      .filter((country: any) => 
+      .filter((country) => 
         country.idd && 
         country.idd.root && 
         country.idd.suffixes && 
         country.idd.suffixes.length > 0
       )
-      .map((country: any) => ({
+      .map((country) => ({
         name: country.name.common,
         code: country.name.common.substring(0, 2).toUpperCase(),
         dialCode: `${country.idd.root}${country.idd.suffixes[0]}`,
         flag: country.flags.svg,
       }))
-      .sort((a: Country, b: Country) => a.name.localeCompare(b.name));
+      .sort((a, b) => a.name.localeCompare(b.name));
   } catch (error) {
     console.error('Error fetching countries:', error);
     
-    // Return some default countries if API fails
+    // Return default countries
     return [
       { name: 'United States', code: 'US', dialCode: '+1', flag: 'https://flagcdn.com/us.svg' },
       { name: 'India', code: 'IN', dialCode: '+91', flag: 'https://flagcdn.com/in.svg' },
@@ -37,4 +61,4 @@ export const fetchCountries = async (): Promise<Country[]> => {
       { name: 'Australia', code: 'AU', dialCode: '+61', flag: 'https://flagcdn.com/au.svg' },
     ];
   }
-}; 
+};
