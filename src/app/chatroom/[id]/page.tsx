088d2d4ad395e51506ea756a6fae68a12752a03e
Mayur Chaudhari <2019mayur.chaudhari@ves.ac.in>
2025-07-17 21:21:21 +0530

    deployment update2

--- src/app/chatroom/[id]/page.tsx
+++ src/app/chatroom/[id]/page.tsx
@@ -1,5 +1,6 @@
 import ChatroomPageClient from '../../../components/chatroom/ChatroomPageClient';
 
 export default async function Page({ params }: { params: { id: string } }) {
+  // No need to manually await params here unless you're doing async logic
   return <ChatroomPageClient id={params.id} />;
 }
